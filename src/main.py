61125541f74bc5b11b3524885e1567e6148dcf00
Anthea Liles <120063455+AntheaLiles@users.noreply.github.com>
2024-10-16 20:02:14 +0200

    speed up + db schema

--- src/main.py
+++ src/main.py
@@ -1,9 +1,20 @@
 from grist_api import get_all_grist_data
 from update_sqlite import update_sqlite
+from sqlalchemy import create_engine
+from sqlalchemy.schema import MetaData
+from sqlalchemy_schemadisplay import create_schema_graph
+
+def generate_db_schema():
+    engine = create_engine('sqlite:///data/project.db')
+    metadata = MetaData()
+    metadata.reflect(bind=engine)
+    graph = create_schema_graph(metadata=metadata)
+    graph.write_svg('data/db_schema.svg')
 
 def main():
     all_data = get_all_grist_data()
     update_sqlite(all_data)
+    generate_db_schema()
 
 if __name__ == "__main__":
     main()
